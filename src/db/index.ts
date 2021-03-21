2599898c34c56e33fdbc9d1c3c952c6f30ca6e6b
John Leo Claudio <johnleoclaudio@users.noreply.github.com>
2021-03-21 20:26:09 +0800

    update eslint and prettier

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,8 @@
-import { Sequelize } from "sequelize";
-import config from "config";
+import { Sequelize } from 'sequelize'
+import config from 'config'
 
 const { username, database: dbName, password, ...dbConfig } = config.get(
-  "database"
-);
+  'database',
+)
 
-export default new Sequelize(dbName, username, password, dbConfig);
+export default new Sequelize(dbName, username, password, dbConfig)
